ede99862a2ea2fa596f1b84b01fe5b4e0290265d
Asier Garrastazu <asier.gc20072gmail.com>
2026-04-05 13:06:31 +0200

    Codigo inicial

---
+++ entregas/garrastazuAsier/src/cine.java
@@ -0,0 +1,43 @@
+class Cine {
+    
+    public String datosPeliculas[][];
+    public String salasTotales[];
+    public String asientosTotales[];
+    public String asientosYaVendidios[];
+    public String fechasDisponibles[];
+    public String horasDidponibles[];
+    public String precio[];
+
+    public void numeroDeEntradasVendidas(String asientosTotales, String asientosYaVendidos) {
+    }
+    
+    public void calcullarRecaudacion(String numeroEntradasVendidas, String precio){
+    }
+    
+    public void asignarProyeccion(String datosPeliculas, String horaDisponible, String fechaDisponible, String salaDisponible) {
+    }
+
+    public void crearCartelera(String asignarProyeccion) {
+    }
+
+    public void comprarEntrada(String cartelera, String precio) {
+    }
+
+    public void mapearAsientos(String asientosTotales, String asientosYaVendidos) {
+    }
+
+    public void seleccionarAsiento(String numeroAsiento) {
+    }
+
+    public void mostrarCartelera(String crearCartelera) {
+    }
+
+        public static void main(String[] args) {
+        }
+
+
+
+
+
+}
+
