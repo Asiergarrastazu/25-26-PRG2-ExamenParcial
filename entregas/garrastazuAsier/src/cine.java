14c83b2e62386c78279cb19b9bfa310fea3da55d
Asier Garrastazu <asier.gc20072gmail.com>
2026-04-07 09:31:05 +0200

    ajustamos los datos en el main para que se asemeje a los del ejemplo dado en el examen

--- entregas/garrastazuAsier/src/cine.java
+++ entregas/garrastazuAsier/src/cine.java
@@ -1,38 +1,37 @@
 class cine {
-    
+
     public String datosPeliculas[][];
     public String salasTotales[];
     public String asientosTotales[];
-    public String asientosVendidios[];
+    public String asientosYaVendidos[];
     public String fechasDisponibles[];
-    public String horasDidponibles[];
+    public String horasDisponibles[];
     public String precio[];
 
     public void numeroEntradasVendidas(String asientosTotales, String asientosVendidos) {
-        int totalAsientos = Integer.parseInt(asientosTotales);
-        int asientosYaVendidos = Integer.parseInt(asientosVendidos);
-
-        System.out.println("Vendidas: " + asientosYaVendidos + "/" + totalAsientos);
+        int total = Integer.parseInt(asientosTotales);
+        int vendidos = Integer.parseInt(asientosVendidos);
+        System.out.println(vendidos + " entradas vendidas de " + total);
     }
-    
-    public void calcularRecaudacion(String numeroEntradasVendidas, String precio){
+
+    public void calcularRecaudacion(String numeroEntradasVendidas, String precio) {
         int entradas = Integer.parseInt(numeroEntradasVendidas);
         double p = Double.parseDouble(precio);
-        System.out.println("Recaudación: " + (entradas * p));
+        double total = entradas * p;
+        System.out.println((int) total + "€");
     }
-    
-    public void asignarProyeccion(String datosPeliculas, String horaDisponible, String fechaDisponible, String salaDisponible) {
-        System.out.println(datosPeliculas + " - " + salaDisponible + " - " + horaDisponible);
+
+    public void asignarProyeccion(String pelicula, String hora, String fecha, String sala) {
+        System.out.println("\"" + pelicula + "\" - " + sala + " - " + fecha + " " + hora);
     }
 
     public void crearCartelera(String asignarProyeccion) {
-        System.out.println("Cartelera: " + asignarProyeccion);
-    }
+        System.out.println(asignarProyeccion);
+}
 
-    public void comprarEntrada(String cartelera, String precio) {
-        double p = Double.parseDouble(precio);
-        System.out.println("Compra: " + cartelera + " -> " + p + "€");
-    }
+    public void mostrarCartelera(String crearCartelera) {
+        System.out.println(crearCartelera);
+}
 
     public void mapearAsientos(String asientosTotales, String asientosYaVendidos) {
         int total = Integer.parseInt(asientosTotales);
@@ -41,28 +40,53 @@ class cine {
         for (int i = 0; i < total; i++) {
             if (i < vendidos) System.out.print("[X]");
             else System.out.print("[ ]");
-    }
-    System.out.println();
+        }
+        System.out.println();
     }
 
     public void seleccionarAsiento(String numeroAsiento) {
-        System.out.println("Asiento: " + numeroAsiento);
+        System.out.println("Asiento seleccionado: " + numeroAsiento);
     }
 
-    public void mostrarCartelera(String crearCartelera) {
-        System.out.println("Mostrando: " + crearCartelera);
+    public void comprarEntrada(String cartelera, String precio) {
+        System.out.println("Compra realizada para " + cartelera + " por " + precio + "€");
     }
 
     public static void main(String[] args) {
+
         cine cine = new cine();
-        
-        cine.numeroEntradasVendidas("100", "45");
-        cine.calcularRecaudacion("45", "8.5");
-        cine.asignarProyeccion("Avatar", "20:00", "10/04/2026", "Sala 1");
-        cine.crearCartelera("Avatar - Sala 1 - 20:00");
-        cine.mostrarCartelera("Avatar - Sala 1 - 20:00");
-        cine.mapearAsientos("10", "4");
-        cine.seleccionarAsiento("5");
-        cine.comprarEntrada("Avatar - Sala 1 - 20:00", "8.5");
-        }
+
+        System.out.println("Cine: Cinepolis Centro\n");
+
+        System.out.println("Películas:");
+        System.out.println("\"Dune: Parte 2\" – Denis Villeneuve – 166 min – +13");
+        System.out.println("\"Oppenheimer\" – Christopher Nolan – 180 min – +16\n");
+
+        System.out.println("Salas:");
+        System.out.println("Sala 1 – Capacidad: 100 asientos");
+        System.out.println("Sala 2 – Capacidad: 150 asientos\n");
+
+        System.out.println("Proyecciones:");
+        cine.asignarProyeccion("Dune: Parte 2", "18:00", "01/04/2025", "Sala 1");
+        System.out.print(" – 10€ – ");
+        cine.numeroEntradasVendidas("100", "85");
+
+        cine.asignarProyeccion("Oppenheimer", "20:00", "02/04/2025", "Sala 2");
+        System.out.print(" – 12€ – ");
+        cine.numeroEntradasVendidas("150", "120");
+
+        System.out.println("\nRecaudación:");
+        System.out.print("Sala 1: ");
+        cine.calcularRecaudacion("85", "10");
+
+        System.out.print("Sala 2: ");
+        cine.calcularRecaudacion("120", "12");
+
+        System.out.println("\nMapa de asientos Sala 1:");
+        cine.mapearAsientos("20", "5");
+
+        cine.seleccionarAsiento("6");
+
+        cine.comprarEntrada("Dune: Parte 2 - Sala 1 - 18:00", "10");
+    }
 }
